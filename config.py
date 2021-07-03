4686aec8a1b97b27fd9c40efe1cd8d0ea7f09def
Javier Gutierrez <jjavierweb@outlook.com>
2021-07-03 12:40:53 -0500

    added posts to model

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
   SECRET_KEY = os.environ.get('SECRECT_KEY') or 'the mass is lower than the fire'
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS =  False

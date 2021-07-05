d0aecdcd1b07972706eb0de8c164a2060119b14d
Javier Gutierrez <jjavierweb@outlook.com>
2021-07-04 21:22:15 -0500

    completed login functionality with database login

--- app/models.py
+++ app/models.py
@@ -1,17 +1,17 @@
-from enum import unique
-
+from app import login
 from sqlalchemy.orm import backref
 from app import db  
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(64), index=True, unique=True)
   email = db.Column(db.String(120), index=True, unique=True)
   password_hash = db.Column(db.String(128))
   posts = db.relationship('Post', backref='author', lazy='dynamic')
-
+    
   def __repr__(self):
       return f'<User {self.username} >'
 
@@ -22,6 +22,7 @@ class User(db.Model):
   # create funtion to compare password hash
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
+
 class Post(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   body = db.Column(db.String(140))
@@ -30,3 +31,7 @@ class Post(db.Model):
 
   def __repr__(self):
       return f'<Post {self.body}>'
+
+@login.user_loader
+def load_user(id):
+  return User.query.get(int(id))

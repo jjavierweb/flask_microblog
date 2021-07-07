6e5b01e90e47fc837f089cf9e4cfae132b9fed16
Javier Gutierrez <jjavierweb@outlook.com>
2021-07-07 00:31:00 -0500

    added error handling for 404 and 500 errors

--- config.py
+++ config.py
@@ -10,3 +10,11 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'the mass is lower than the fire'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Email server configuration
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['jjavier@outlook.com']

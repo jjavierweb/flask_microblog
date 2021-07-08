1bedbc02a5a6271710893240d2aeb8d8852e8c5f
Javier Gutierrez <jjavierweb@outlook.com>
2021-07-07 22:03:46 -0500

    fixed some naming issues and added file logging

--- app/errors.py
+++ app/errors.py
@@ -4,7 +4,7 @@ from app import app, db
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return  render_template('404.html'), 400
+    return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_error(error):
